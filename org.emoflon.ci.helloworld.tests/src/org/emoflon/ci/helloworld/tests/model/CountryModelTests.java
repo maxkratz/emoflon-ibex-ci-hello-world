c63791d02a4e700b45963b92db1da3ab7597634f
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-03-03 16:14:50 +0100

    Adds countries to metamodel + tests
    Collection fixes

--- org.emoflon.ci.helloworld.tests/src/org/emoflon/ci/helloworld/tests/model/CountryModelTests.java
+++ org.emoflon.ci.helloworld.tests/src/org/emoflon/ci/helloworld/tests/model/CountryModelTests.java
@@ -1,18 +1,14 @@
-package org.emoflon.ci.helloworld.tests.world;
+package org.emoflon.ci.helloworld.tests.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+import helloworld.Country;
 import helloworld.HelloworldFactory;
-import helloworld.World;
 
-/**
- * This class tests the generated code of the class World (defined within the
- * metamodel).
- */
-public class WorldTests {
+public class CountryModelTests {
 
 	@Test
 	public void testHelloworldFactoryInstanceNotNull() {
@@ -21,9 +17,11 @@ public class WorldTests {
 
 	@Test
 	public void testGetterSetter() {
-		final World w = HelloworldFactory.eINSTANCE.createWorld();
-		w.setCounter(73);
-		assertEquals(73, w.getCounter());
+		final Country c = HelloworldFactory.eINSTANCE.createCountry();
+		c.setPopulation(42);
+		assertEquals(42, c.getPopulation());
+		c.setName("germany");
+		assertEquals("germany", c.getName());
 	}
 
 }
---
+++ org.emoflon.ci.helloworld.tests/src/org/emoflon/ci/helloworld/tests/model/WorldModelTests.java
@@ -0,0 +1,38 @@
+package org.emoflon.ci.helloworld.tests.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.emoflon.smartemf.runtime.collections.LinkedSmartESet;
+import org.junit.jupiter.api.Test;
+
+import helloworld.HelloworldFactory;
+import helloworld.World;
+
+/**
+ * This class tests the generated code of the class World (defined within the
+ * metamodel).
+ */
+public class WorldModelTests {
+
+	@Test
+	public void testHelloworldFactoryInstanceNotNull() {
+		assertNotNull(HelloworldFactory.eINSTANCE);
+	}
+
+	@Test
+	public void testGetterSetter() {
+		final World w = HelloworldFactory.eINSTANCE.createWorld();
+		w.setCounter(73);
+		assertEquals(73, w.getCounter());
+	}
+
+	@Test
+	public void testNoCountries() {
+		final World w = HelloworldFactory.eINSTANCE.createWorld();
+
+		// No countries in newly created world
+		assertEquals(0, w.getCountries().size());
+	}
+
+}
